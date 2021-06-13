69529e8b4bca17e5adf89d774327bb57532d345f
nithin113 <85840872+nithin113@users.noreply.github.com>
2021-06-13 22:24:34 +0530

    Add files via upload

---
+++ a.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>random</title>
+</head>
+<body> <p> random number is
+<?php
+$choice = rand(1,100);
+echo $choice;
+?>
+and its square root is
+<?php
+echo sqrt($choice);
+?>
+</p>
+</body>
+</html>
